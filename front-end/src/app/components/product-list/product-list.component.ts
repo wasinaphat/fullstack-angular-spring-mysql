71dd078c7de8a9e775e4149e4099d1401299a026
wasinaphat <wasinapl@scg.com>
2021-04-19 03:54:26 +0700

    prepare header and   footer

---
+++ front-end/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,25 @@
+import { Product } from './../../common/product';
+import { ProductService } from './../../services/product.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list-grid.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products:Product[];
+  constructor(private productService:ProductService) { }
+
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts(){
+    this.productService.getProductList().subscribe(data=>{
+      this.products=data;
+    });
+  }
+
+}
